816c03d964f583eff41377f2dc46558730ef07ce
Jakub <kubasikora00@gmail.com>
2026-05-26 13:50:20 +0200

    Dodanie formularza dodawnia agenta

--- frontend/src/app/pages/admin-agents/admin-agents.ts
+++ frontend/src/app/pages/admin-agents/admin-agents.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-agents',
-  imports: [NgFor],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './admin-agents.html',
   styleUrl: './admin-agents.css'
 })
 export class AdminAgents {
+  showForm = false;
+
   agents = [
     {
       firstName: 'Jan',
@@ -33,7 +36,57 @@ export class AdminAgents {
     }
   ];
 
+  newAgent = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    status: 'Aktywny'
+  };
+
   constructor(private router: Router) {}
+
+  showAddAgentForm(): void {
+    this.showForm = true;
+  }
+
+  addAgent(): void {
+    if (
+      !this.newAgent.firstName ||
+      !this.newAgent.lastName ||
+      !this.newAgent.email ||
+      !this.newAgent.phone
+    ) {
+      alert('Uzupełnij wszystkie pola formularza.');
+      return;
+    }
+
+    this.agents.push({
+      firstName: this.newAgent.firstName,
+      lastName: this.newAgent.lastName,
+      email: this.newAgent.email,
+      phone: this.newAgent.phone,
+      status: this.newAgent.status
+    });
+
+    this.clearForm();
+    this.showForm = false;
+  }
+
+  cancel(): void {
+    this.clearForm();
+    this.showForm = false;
+  }
+
+  clearForm(): void {
+    this.newAgent = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      status: 'Aktywny'
+    };
+  }
 
   goBack(): void {
     this.router.navigate(['/admin']);
